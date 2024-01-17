266fcf20b1baf17dc8e60f820b0c028ee72151bc
Holly Rees <hollyclarerees@gmail.com>
2024-01-17 12:46:17 +0000

    add change to connection.js

--- db/connection.js
+++ db/connection.js
@@ -1,18 +1,19 @@
 const { Pool } = require("pg");
 const ENV = process.env.NODE_ENV || "development";
 
-if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
-  throw new Error("PGDATABASE or DATABASE_URL not set");
-}
-
 const config = {};
 
+require("dotenv").config({
+  path: `${__dirname}/../.env.${ENV}`,
+});
+
 if (ENV === "production") {
-  require("dotenv").config({
-    path: `${__dirname}/../.env.${ENV}`,
-  });
   config.connectionString = process.env.DATABSE_URL;
   config.max = 2;
+}
+
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error("PGDATABASE or DATABASE_URL not set");
 }
 
 module.exports = new Pool(config);
