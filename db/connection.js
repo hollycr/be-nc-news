9f9aae45fe287d56e28c30ca2927b3ead0a1e972
Holly Rees <hollyclarerees@gmail.com>
2024-01-17 12:42:10 +0000

    update connection config

--- db/connection.js
+++ db/connection.js
@@ -1,10 +1,6 @@
 const { Pool } = require("pg");
 const ENV = process.env.NODE_ENV || "development";
 
-require("dotenv").config({
-  path: `${__dirname}/../.env.${ENV}`,
-});
-
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error("PGDATABASE or DATABASE_URL not set");
 }
@@ -12,6 +8,9 @@ if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
 const config = {};
 
 if (ENV === "production") {
+  require("dotenv").config({
+    path: `${__dirname}/../.env.${ENV}`,
+  });
   config.connectionString = process.env.DATABSE_URL;
   config.max = 2;
 }
