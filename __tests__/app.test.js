f835ad1560504b48500ecf785086747f49ea90f5
Holly Rees <hollyclarerees@gmail.com>
2024-01-17 10:04:15 +0000

    add endpoint for GET /api/users

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -355,3 +355,21 @@ describe("/api/comments", () => {
     });
   });
 });
+
+describe("/api/users", () => {
+  test("GET: 200 responds with an array of all users", () => {
+    return request(app)
+      .get("/api/users")
+      .expect(200)
+      .then(({ body }) => {
+        expect(body.users.length).toBe(4);
+        body.users.forEach((user) => {
+          expect(user).toMatchObject({
+            username: expect.any(String),
+            name: expect.any(String),
+            avatar_url: expect.any(String),
+          });
+        });
+      });
+  });
+});
