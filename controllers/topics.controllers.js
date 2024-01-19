7d3b2bd1060386818e64fa68c6d1fa553d159d5a
Holly Rees <hollyclarerees@gmail.com>
2024-01-19 17:43:20 +0000

    add endpoint POST /api/topics

--- controllers/topics.controllers.js
+++ controllers/topics.controllers.js
@@ -1,9 +1,20 @@
-const { fetchTopics } = require("../models/topics.models");
+const { fetchTopics, insertTopic } = require("../models/topics.models");
 
 module.exports.getTopics = (req, res, next) => {
   fetchTopics()
     .then((topics) => {
       res.status(200).send({ topics });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
+
+module.exports.postTopic = (req, res, next) => {
+  const { slug, description } = req.body;
+  insertTopic(slug, description)
+    .then((topic) => {
+      res.status(201).send({ topic });
     })
     .catch((err) => {
       next(err);
--- models/topics.models.js
+++ models/topics.models.js
@@ -5,3 +5,16 @@ module.exports.fetchTopics = () => {
     return rows;
   });
 };
+
+module.exports.insertTopic = (slug, description) => {
+  const values = [slug, description];
+  if (slug === "")
+    return Promise.reject({
+      status: 400,
+      msg: "Bad Request: slug cannot be an empty string",
+    });
+  const queryStr = `INSERT INTO topics (slug, description) VALUES ($1, $2) RETURNING *;`;
+  return db.query(queryStr, values).then(({ rows }) => {
+    return rows[0];
+  });
+};
